32c536051955bde0624229b2f5c072f694cbaac5
jefer <yefersontejeda@gmail.com>
2026-09-15 17:35:20 -0400

    Separa operaciones en modulo independiente

---
+++ operaciones.py
@@ -0,0 +1,43 @@
+# Lista de operaciones, donde cada operacion es un diccionario.
+operaciones = [
+	{
+		"simbolo": "AAPL",
+		"ticker": "AAPL",
+		"mercado": "acciones",
+		"precio_entrada": 150,
+		"precio_actual": None,
+		"cantidad": 10,
+	},
+	{
+		"simbolo": "BTC",
+		"ticker": "BTC-USD",
+		"mercado": "criptomonedas",
+		"precio_entrada": 60000,
+		"precio_actual": None,
+		"cantidad": 0.05,
+	},
+	{
+		"simbolo": "EURUSD",
+		"ticker": "EURUSD=X",
+		"mercado": "forex",
+		"precio_entrada": 1.10,
+		"precio_actual": None,
+		"cantidad": 1000,
+	},
+	{
+		"simbolo": "TSLA",
+		"ticker": "TSLA",
+		"mercado": "acciones",
+		"precio_entrada": 250,
+		"precio_actual": None,
+		"cantidad": 4,
+	},
+	{
+		"simbolo": "MSFT",
+		"ticker": "MSFT",
+		"mercado": "acciones",
+		"precio_entrada": 400,
+		"precio_actual": None,
+		"cantidad": 5,
+	},
+]
